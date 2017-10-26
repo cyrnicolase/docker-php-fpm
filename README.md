fb144c54db0597de3dfbb5ab71c07e62ce137f8e
chenyarong <chenyarong@haochang.tv>
2017-10-26 12:23:41 +0800

    不暴露端口，通过--link 的方式，将容器共享

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ A dockerfile of php-fpm
 # Notice
 
 Before run the script run.sh. We should take the etc files into /opt/php-7.1.10/etc.
+
+
--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 docker run --detach \
-    --publish 9001:9000 \
     --name php-fpm \
     --restart always \
     --volume /opt/php-7.1.10/etc:/usr/local/etc \
+    --volume /home/chenyarong/sites/vod-ci:/var/www/html:ro \
     php-fpm:7.1.10
+
+# --publish 9001:9000
 
